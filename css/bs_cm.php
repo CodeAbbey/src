3ead9ad60550c797b138849840f1ccd2c195dfe1
Rodion <rodiongork@github.com>
2024-06-28 10:37:31 +0300

    Minor improvements to aggregated scripts/styles

--- css/bs_cm.php
+++ css/bs_cm.php
@@ -1,5 +1,6 @@
 <?php
 header('Content-Type: text/css');
+header('Expires: '.gmdate('D, d M Y H:i:s \G\M\T', time() + (60 * 60)));
 echo file_get_contents('bootstrap.min.css') . "\n\n";
 echo file_get_contents('bootstrap-theme.min.css') . "\n\n";
 echo file_get_contents('common_bs.css') . "\n\n";
--- js/jq_bs_cm.php
+++ js/jq_bs_cm.php
@@ -1,5 +1,6 @@
 <?php
 header('Content-Type: application/javascript');
+header('Expires: '.gmdate('D, d M Y H:i:s \G\M\T', time() + (60 * 60)));
 echo file_get_contents('jq.js') . "\n\n";
 echo file_get_contents('bootstrap.min.js') . "\n\n";
 echo file_get_contents('common.js') . "\n\n";
